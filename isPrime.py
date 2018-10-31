94b838efdddf985ed439be58361cb7664affd969
Gdango <vthhuynh@ucdavis.edu>
2018-10-30 21:14:25 -0700

    First commit

--- isPrime.py
+++ isPrime.py
@@ -1,5 +1,5 @@
 def isPrime(value):
-    for i in range(2,10):
+    for i in range(2,10): # prime number should not be able to divide by any number between 2-10
         if value % i == 0:
             print("Number",value, "is not a prime number!")
             return
